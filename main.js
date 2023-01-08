1f210898750af8cdc40cc93036b158f8c410e97f
Abdulloh <abdullohkurbanbayev@gmail.com>
2023-01-08 18:01:15 +0500

    123

---
+++ main.js
@@ -0,0 +1,83 @@
+/*** console.log() metodi ***/
+
+// console.log('Hello Console');
+// console.error('Error');
+// console.warn('Warn');
+
+
+/*** Malumot Turlari ***/
+
+// console.log('String 22'); /* String - matin malumot turi  */
+// console.log(777); /* Number - son malumot turi  */
+// console.log(NaN); /* NaN - Not a Number - Son Emas   */
+// console.log(Infinity); /* Cheksiz  */
+// console.log(true); /* Boolean - Mantiqiy malumot turi (true = rost) */
+// console.log(false); /* Boolean - Mantiqiy malumot turi (false = yolg'on) */
+// console.log(null); // null - bosh malumot turi
+// console.log(undefined); // undefined - aniqlanmagan malumot turi
+
+
+/*** Ozgaruvchanlar ***/
+
+// biz ozgaruvchan yaratishimiz uchun bizga mahsus var kaliti kerak boladi
+
+// var myName = 'AbdulMalik';
+// var myAge = 23;
+// var myJob = 'Web Developer'
+
+// // console.log('Mening ismim ' + myName + ' mening yoshim ' + myAge + ' da' + ' mening kasbim ' + myJob);
+// /* malumotlarni qoshish usuli yaniy Konketonatsiya  */
+
+// var allInfo = 'Mening ismim ' + myName + ' mening yoshim ' + myAge + ' da' + ' mening kasbim ' + myJob
+
+// console.log(allInfo);
+
+// Operatorlar
+
+// console.log(6 + 3);
+// console.log(5 - 2);
+// console.log(10 / 2);
+// console.log(2 * 2);
+// console.log(10 % 2);
+
+
+// console.log('6 + 3 = ' + (6 + 3));
+// console.log('5 - 2 = ' + (5 - 2));
+// console.log('10 / 2 = ' + (10 / 2));
+// console.log('2 * 2 = ' + (2 * 2));
+// console.log('9 % 2 = ' + (9 % 2));
+
+
+// var str = prompt('Ismingizni kiriting')
+/* prompt brauzerda clientdan malumot kiritishini soraydi va usha malumot str ozgaruvchanida saqlab qolinadi */
+// alert(str)
+// console.log(str);
+/* alert malumotni brauzerda korsatadi faqat alohida oynada. */
+
+// var num = +prompt('Son kiriting')
+// alert(num)
+// console.log(num);
+
+// UYGA VAZIFA
+
+var str = prompt('Ismingizni Kriting')
+console.log(str);
+
+var num = +prompt('Yoshingizni Kriting')
+console.log(num);
+
+var str = ( 'Misollarni Yeching')
+alert (str)
+
+var num = +prompt ('6 + 3')
+console.log('6 + 3 ='+ num);
+
+var num = +prompt ('5 - 2')
+console.log('5 - 2 ='+ num);
+
+var num = +prompt ('10 / 2)')
+console.log('10 / 2='+ num);
+
+var num = +prompt ('2 * 2')
+console.log('2 * 2 ='+ num);
+
